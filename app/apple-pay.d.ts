d0a5cefe2e4cdf5541ad88092fada604712f344b
Jess <jessbodie@gmail.com>
2026-07-15 14:52:26 -0400

    Display Apple button

---
+++ app/apple-pay.d.ts
@@ -0,0 +1,18 @@
+import type { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'apple-pay-button': DetailedHTMLProps<
+        HTMLAttributes<HTMLElement> & {
+          buttonstyle?: 'black' | 'white' | 'white-outline';
+          type?: 'plain' | 'buy' | 'tip' | 'checkout';
+          locale?: string;
+        },
+        HTMLElement
+      >;
+    }
+  }
+}
+
+export {};
